cc0a4910fc62301ad6c9b78686034c81e063771f
unknown <yunusyerli1@gmail.com>
2020-05-26 23:07:57 +0300

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import data from './data';
+import {BowserRouter, Route} from 'react-router-dom';
+
+function App() {
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("open");
+  }
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("open");
+  }
+  return (
+    <BrowserRouter>
+    <div className="grid-container">
+
+
+    <header className="header">
+        <div className="brand">
+            <button onClick={openMenu}>
+                &#9776;
+            </button>
+            <a href="index.html">Amazona</a>
+        </div>
+        <div className="header-links">
+            <a href="cart.html">Cart</a>
+            <a href="signin">Signin</a>
+        </div>
+    </header>
+
+    <aside className="sidebar">
+        <h3>Shopping Categories</h3>
+        <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+        <ul>
+            <li><a href="index.html">Pants</a></li>
+            <li><a href="index.html"></a>Shirts</li>
+        </ul>
+    </aside>
+    <main className="main">
+        <div className="content">
+          <Route path="/products/:id" component={ProductScreen} />
+          <Route path="/" exact component={HomeScreen} />
+            <ul className="products">
+              {
+                data.products.map(product =>
+              
+                <li>
+                    <div className="product">
+                        <img className="product-image" src={product.image}  alt="product1" />
+                        <div className="product-name">
+                            <a href="product.html">{product.name}</a>
+                        </div>
+                        <div className="product-brand">{product.brand}</div>
+                        <div className="product-price">{product.price}</div>
+                        <div className="product-rating">{product.rating} Stars ({product.numReviews} Reviews)</div>
+                    </div>
+                </li>)
+              }
+            </ul>
+        </div>
+    </main>
+
+
+
+    <footer className="footer">
+        All right reserved
+    </footer>
+
+
+</div>
+</BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,59 @@
+export default  {
+    products:[
+        {
+            name: 'Slim Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10
+         },
+         {
+            name: 'Best Pants',
+            category: 'Pants',
+            image: '/images/d1.jpg',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10
+         },
+         {
+            name: 'Fit Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10
+         },
+         {
+            name: 'Slim Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10
+         },
+         {
+            name: 'Best Pants',
+            category: 'Pants',
+            image: '/images/d1.jpg',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10
+         },
+         {
+            name: 'Fit Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10
+         }
+         
+]
+}
