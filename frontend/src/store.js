b16213d8155853590a5ade1af8dc0f2d9f653c6b
unknown <yunusyerli1@gmail.com>
2020-05-28 02:09:19 +0300

    Cart screen, add To Cart, Remove From Cart, Cookie

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,14 +1,17 @@
 import {createStore, applyMiddleware ,compose, combineReducers} from 'redux';
-import thunk from 'redux-thunk'
+import thunk from 'redux-thunk';
+import Cookie from 'js-cookie';
 import {productListReducer, productDetailsReducer} from './redux/reducers/productReducer';
+import { cartReducer } from './redux/reducers/cartReducer';
 
-
-const initialState = {};
+const cartItems = Cookie.getJSON("cartItems") || [];
+const initialState = {cart:{cartItems}};
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const reducer = combineReducers({
     productList: productListReducer,
-    productDetails: productDetailsReducer
+    productDetails: productDetailsReducer,
+    cart:cartReducer
 });
 
 const store = createStore(
